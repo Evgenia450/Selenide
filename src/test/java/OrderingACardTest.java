1bc7e8c51a3729661e08ce85e2e58d55f04cd6c2
sergey.perov <sergey.perov@soft-machine.ru>
2022-12-09 17:56:44 +0300

    update test

--- src/test/java/OrderingACardTest.java
+++ src/test/java/OrderingACardTest.java
@@ -3,9 +3,12 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -20,25 +23,27 @@ public class OrderingACardTest {
     void openURL() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
+
     }
+
     @Test
     void shouldSendARequest() {
+        String planningDate = generateDate(3);
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        form.$("[data-test-id=date] input").doubleClick().sendKeys("Del");
-        form.$("[data-test-id=date] input").setValue(getDate());
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Сергей Перов");
         form.$("[data-test-id=phone] input").setValue("+79002073131");
         form.$("[data-test-id=agreement]").click();
         form.$("[class=button__text]").click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 
-    public String getDate() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_MONTH, +3);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        return dateFormat.format(calendar.getTime());
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
